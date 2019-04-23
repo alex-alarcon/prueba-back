c15311a3da0fa53e97dadc154a48d6565644fdcf
Alexander Alarcon <walarcon14@gmail.com>
2019-04-23 16:43:37 -0500

    Se maneja respuesta de la ruta /mi-servicio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/mi-servicio', function(req, res, next) {
+  res.status(200);
+  res.json(
+    [
+      {id: 1, name: 'Boeing'},
+      {id: 2, name: 'Airbus'}
+    ]
+  );
 });
 
 module.exports = router;
